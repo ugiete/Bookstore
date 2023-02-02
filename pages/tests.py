687023b75fc1676d64042701c2ae140784f96b00
Leonardo Ugiete <leonardougiete@hotmail.com>
2023-02-02 14:17:00 -0700

    Create about page

--- pages/tests.py
+++ pages/tests.py
@@ -19,3 +19,20 @@ class HomePageTests(SimpleTestCase):
     def test_resolver(self):
         view = resolve("/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("about")
+        self.response = self.client.get(url)
+
+    def test_location(self):
+        self.assertEqual(self.response.status_code, 200)
+    
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+        self.assertContains(self.response, "About page")
+        self.assertNotContains(self.response, "Hi there! I should not be on the page.")
+
+    def test_resolver(self):
+        view = resolve("/about/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
