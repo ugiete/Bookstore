3bacb01adf50e17c496efd2e12346abfee9b6d7b
Leonardo Ugiete <leonardougiete@hotmail.com>
2023-02-03 00:44:29 -0700

    Add book reviews

--- books/models.py
+++ books/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 from uuid import uuid4
@@ -12,5 +13,12 @@ class Book(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse("book_detail", args=[str(self.id)])
-    
+        return reverse('book_detail', args=[str(self.id)])
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    review = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.review
