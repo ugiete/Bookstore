72e73b55901517a576abb17e509a2eba14f3c9a1
Leonardo Ugiete <leonardougiete@hotmail.com>
2023-02-03 10:52:30 -0700

    Update allowed hosts

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['pacific-wildwood-51968.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
